3cfa454c26a4d5a845b2a016a133572bd5aa7bfa
Harish Prasanna <harishperfect@gmail.com>
2015-11-17 18:22:44 -0500

    init

---
+++ avert/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  avert
+//
+//  Created by Harish Prasanna on 11/17/15.
+//  Copyright © 2015 MetaSquare. All rights reserved.
+//
+
+import UIKit
+import Parse
+import ParseUI
+
+class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
